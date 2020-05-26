2439067679dccf2725d65ef1828113a3747462e2
John Dimeo <dimeo@datamininglab.com>
2020-05-25 22:25:57 -0400

    Commit script that post-processes the release plugin so that the master doesn't have a snapshot version

---
+++ src/scripts/win/mvn-release.ps1
@@ -0,0 +1,8 @@
+# mvn release:prepare release:perform
+$curr = ((git rev-parse HEAD) | Out-String).trim()
+$prev = ((git rev-parse HEAD^1) | Out-String).trim()
+git checkout master
+git merge --no-ff $prev -m "Merging release into master"
+git checkout develop
+git merge --no-ff master -m "Merging release into develop"
+git merge --no-ff $curr -m "Preparing develop for next iteration"
