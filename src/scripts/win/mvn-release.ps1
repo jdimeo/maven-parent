be3efb2ab3675bf3c6cabf54e0362f2eb48418f7
John Dimeo <dimeo@elderresearch.com>
2020-08-10 21:12:01 -0400

    Don't do Git commands if maven release failed

--- src/scripts/win/mvn-release.ps1
+++ src/scripts/win/mvn-release.ps1
@@ -1,8 +1,9 @@
 mvn release:prepare release:perform
+if ($?) {
 $curr = ((git rev-parse HEAD) | Out-String).trim()
 $prev = ((git rev-parse HEAD^1) | Out-String).trim()
 git checkout master
 git merge --no-ff $prev -m "Merging release into master"
 git checkout develop
-git merge --no-ff master -m "Merging release into develop"
 git merge --no-ff $curr -m "Preparing develop for next iteration"
+}
